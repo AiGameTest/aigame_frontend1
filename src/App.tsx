f12f418ae6fd1e1d7dce9ff2c8d742ec29358849
rbxo0128 <rbxo0128@naver.com>
2026-02-18 18:10:08 +0900

    feat: AI 모드 및 사건 상세 페이지 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+﻿import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
 import { AppLayout } from './components/AppLayout';
@@ -7,12 +7,10 @@ import { AdminRoute } from './routes/AdminRoute';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
 import { OAuthCallbackPage } from './pages/OAuthCallbackPage';
-import { CasePage } from './pages/CasePage';
 import { PlayPage } from './pages/PlayPage';
 import { ResultPage } from './pages/ResultPage';
 import { CreatePage } from './pages/CreatePage';
 import { ProfilePage } from './pages/ProfilePage';
-import { AiModePage } from './pages/AiModePage';
 import { CoinShopPage } from './pages/CoinShopPage';
 import { AdminPage } from './pages/AdminPage';
 import { AdminCaseEditPage } from './pages/AdminCaseEditPage';
@@ -32,8 +30,6 @@ export default function App() {
         <Route path="/cases" element={<CaseBrowsePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/oauth/:provider/callback" element={<OAuthCallbackPage />} />
-        <Route path="/ai" element={<ProtectedRoute><AiModePage /></ProtectedRoute>} />
-        <Route path="/case/:id" element={<ProtectedRoute><CasePage /></ProtectedRoute>} />
         <Route path="/play/:sessionId" element={<ProtectedRoute><PlayPage /></ProtectedRoute>} />
         <Route path="/result/:sessionId" element={<ProtectedRoute><ResultPage /></ProtectedRoute>} />
         <Route path="/create" element={<ProtectedRoute><CreatePage /></ProtectedRoute>} />
